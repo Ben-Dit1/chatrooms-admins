7d296aeed8d4bc94577b9815dbd6357c9d060e43
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-06-30 16:18:27 +0300

    feat: qr screen modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import { Inter } from 'next/font/google';
-
+import Image from 'next/image';
+import ethBarcelona from '@/public/assets/logo.png';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main className="flex justify-center w-full">
-      <p>Welcome to dashboard</p>
+    <main className="flex justify-center items-center w-full min-h-[92vh] bg">
+      <Image
+        src={ethBarcelona.src}
+        alt="ethbarcelona"
+        width={400}
+        height={400}
+      />
     </main>
   );
 }
--- pages/sessions/key/[id].tsx
+++ pages/sessions/key/[id].tsx
@@ -5,6 +5,12 @@ import Router, { useRouter } from 'next/router';
 import { useCreateKey } from '@/hooks/queries/useCreateKey';
 import { error } from '@/hooks/useToastify';
 import { APP_AUTH_ROOT } from '@/config';
+import Image from 'next/image';
+import logo from '@/public/assets/logo.png';
+import chatroomsLogo from '@/public/assets/chatroomsLogo.svg';
+import ethyleneBig from '@/public/assets/ethyleneBig.png';
+import doingudBig from '@/public/assets/doingudBig.png';
+import orbisBig from '@/public/assets/orbisBig.png';
 
 export default function Qr() {
   const [link, setLink] = useState<string>('');
@@ -33,14 +39,51 @@ export default function Qr() {
 
   return (
     <>
-      <div className="bg-gray-900 w-full h-screen min-h-fullscreen flex items-center justify-center relative z-10">
-        <div className="md:space-x-16 flex flex-col items-center justify-center md:flex-row space-y-8 md:space-y-0 py-5 md:py-0">
-          <div className="flex justify-center items-center flex-col text-white space-y-4">
-            <img
-              className="w-[200px] rounded-md drop-shadow-xl shadow-sm shadow-gray-800"
-              src={link}
-              alt="qrToMint"
-            />
+      <div className="bg w-full space-x-0 space-y-4 md:space-x-10 md:space-y-0 h-screen min-h-fullscreen flex-col md:flex-row flex items-center justify-center z-10">
+        <img
+          className="w-[40%] lg:w-[26%] rounded-3xl drop-shadow-xl shadow-sm shadow-gray-800"
+          src={link}
+          alt="qrToMint"
+        />
+
+        <div className="w-[56%] lg:w-[30%] min-h-[50%] border-[#283040] border-4 rounded-3xl linear-card flex flex-col justify-between items-center text-white py-8 px-6">
+          <Image
+            alt="chatroomsLogo"
+            src={chatroomsLogo.src}
+            width={250}
+            height={250}
+          />
+          <div className="flex flex-col text-center justify-center items-center space-y-2">
+            <p className="text-3xl font-medium">Engage with speakers</p>
+            <p className="text-xl font-medium">by asking and upvoting</p>
+          </div>
+          <div className="px-4 py-2 rounded-lg border-[1px] border-white text-center">
+            <p className="text-lg">+100$ USDC GIVEAWAY* 🎉</p>
+          </div>
+          <div className="flex items-center justify-between w-[90%]">
+            <div className="flex flex-col space-y-1 items-start">
+              <p className="text-xs font-light">Developed by</p>
+              <div className="flex items-center flex-col space-x-0 space-y-1 md:space-x-2 md:flex-row justify-start">
+                <Image
+                  src={ethyleneBig.src}
+                  alt="ethyleneBig"
+                  width={110}
+                  height={50}
+                />
+                <Image
+                  src={doingudBig.src}
+                  alt="doingudBig"
+                  width={70}
+                  height={30}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-1 items-center">
+              <p className="text-xs font-light">Powered by</p>
+              <div className="flex items-center">
+                <Image alt="" src={orbisBig.src} width={70} height={30} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
