0f2884fa62148a3dc3cb51450c2bfb1755460a68
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2023-07-03 15:50:40 +0300

    feat: char limit to 50

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const APIROOT = 'https://chatrooms-api.ethylene.io';
 export const APP_AUTH_ROOT = 'https://chatrooms.social/auth';
-export const SESSION_LIMIT = 25;
+export const SESSION_LIMIT = 50;
 export const MESSAGE = 'Sign this message to access chatroom features';
 export const ADMIN_ROOT = 'https://admin.chatrooms.social';
